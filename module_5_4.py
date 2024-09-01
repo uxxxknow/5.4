7a5ed0c5111b4f8704e0aa3e6a1822ae93e896e0
uxxxknow <DALVgrin2000@mail.ru>
2024-09-01 22:28:33 +0300

    Готовое

---
+++ module_5_4.py
@@ -0,0 +1,70 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors = value + 10
+        elif isinstance(value, House):
+            value.number_of_floors
+        return self
+
+    def __radd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors = 10 + self.number_of_floors
+        elif isinstance(value, House):
+            value.number_of_floors
+        return self
+
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += 10
+        elif isinstance(value, House):
+            value.number_of_floors
+        return self
+
+    def __del__(self):
+        print(self.name, 'снесён, но он останется в истории')
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
+
